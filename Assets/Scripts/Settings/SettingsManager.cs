dac13ee1756fdc043cc87b48bcf4a418c527221c
Byron Wright <bslayerw@gmail.com>
2021-03-16 01:07:21 -0400

    - Implemented sun surface shader - Start cinemachine follow and target camera implementation along with UI for camera selection.

--- Assets/Scripts/Settings/SettingsManager.cs
+++ Assets/Scripts/Settings/SettingsManager.cs
@@ -23,6 +23,7 @@ using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Utils;
 
 namespace Settings
 {
@@ -30,25 +31,62 @@ namespace Settings
     {
         [SerializeField] public UISettings uiSettings;
         [SerializeField] private Slider speedMultiplierSlider;
-
         [SerializeField] private TextMeshProUGUI currentSpeedValueLabel;
+        [SerializeField] private TMP_Dropdown planetTargetCameraDropdown;
 
+        [SerializeField] private Transform[] cameraTargets = null;
+
+        private LookAtTarget lookAtTargetComponent;
 
         private void OnEnable()
         {
+            lookAtTargetComponent = Camera.main.GetComponent<LookAtTarget>();
             SetupSpeedSlider();
+            SetupPlanetTargetCameraDropdown();
         }
 
         private void SetupSpeedSlider()
         {
             speedMultiplierSlider.value = UISettings.Instance.speedMultiplier;
             currentSpeedValueLabel.text = speedMultiplierSlider.value.ToString("0.00", CultureInfo.InvariantCulture);
-            speedMultiplierSlider.onValueChanged.AddListener (delegate {OnSpeedSliderValueChanged ();});
+            speedMultiplierSlider.onValueChanged.AddListener(delegate { OnSpeedSliderValueChanged(); });
         }
-        
+
+        private void SetupPlanetTargetCameraDropdown()
+        {
+            planetTargetCameraDropdown.value = UISettings.Instance.currentPlanetTargetSelection;
+            planetTargetCameraDropdown.onValueChanged.AddListener(delegate
+            {
+                OnPlanetTargetCameraDropdownValueChanged();
+            });
+        }
+
+        public void OnPlanetTargetCameraDropdownValueChanged()
+        {
+            Debug.Log(
+                $"planet selected: [{planetTargetCameraDropdown.value}]:{planetTargetCameraDropdown.options[planetTargetCameraDropdown.value].text}");
+            if (lookAtTargetComponent is null)
+            {
+                Debug.LogError(
+                    "could not find at LookAtTarget component on the main camera. We have to ignore this. Make sure you have a LookAtTarget script on the main camera"
+                );
+                return;
+            }
+
+            if (cameraTargets is null)
+            {
+                Debug.LogError(
+                    "there are no camera targets. Make sure you've added all the camera look at targets to the camera targets list in the inspector"
+                );
+                return;
+            }
+
+            lookAtTargetComponent.currentTarget = cameraTargets[planetTargetCameraDropdown.value].gameObject;
+        }
+
         public void OnSpeedSliderValueChanged()
         {
-            Debug.Log (speedMultiplierSlider.value);
+            Debug.Log(speedMultiplierSlider.value);
             uiSettings.speedMultiplier = speedMultiplierSlider.value;
             currentSpeedValueLabel.text = speedMultiplierSlider.value.ToString("0.00", CultureInfo.InvariantCulture);
         }
