fa4c5057ef84054713482914eee6d6fd7871ab04
Byron Wright <bslayerw@gmail.com>
2021-03-17 14:34:30 -0400

    - Fixed issue with the sun shader causing strange white flashes.\n - Started writing build pipeline scripts to automate builds across all platforms.

--- Assets/Scripts/Editor/Build/Build.cs
+++ Assets/Scripts/Editor/Build/Build.cs
@@ -1,52 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using Ionic.Zip;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-public class Build
+namespace Editor.Build
 {
-    static string buildPath = $"{Application.dataPath}/../buildfiles";
-    [MenuItem( "DevOps/Perform All Builds" )]
-    static void PerformAllBuilds()
+    public class Build
     {
-        PerformWebGLBuild();
-        PerformMacOSBuild();
-        PerformWindowsBuild();
-        PerformLinuxBuild();
-    }
-
-    [MenuItem( "DevOps/Build/Perform WebGL Build" )]
-    public static void PerformWebGLBuild()
-    {
-        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.webGL", BuildTarget.WebGL, BuildOptions.None);
-    }
-    
-    [MenuItem( "DevOps/Build/Perform macOS Build" )]
-    public static void PerformMacOSBuild()
-    {
-        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.osX/SolarSystem.app", BuildTarget.StandaloneOSX, BuildOptions.None);
-        using(ZipFile zip= new ZipFile())
+        static string buildPath = $"{Application.dataPath}/../buildfiles";
+        [MenuItem( "DevOps/Perform All Builds" )]
+        static void PerformAllBuilds()
         {
-            zip.AddFile("../Builds/SolarSystem.osX/SolarSystem.app");
-            zip.Save("../Builds/SolarSystem.osX/SolarSystem.app.zip");
+            var versionNumber = Git.BuildVersion;
+            PlayerSettings.bundleVersion = versionNumber;
+            Debug.Log($"Build for all platforms, version number {versionNumber}");
+            /*PerformWebGLBuild();
+            PerformMacOSBuild();
+            PerformWindowsBuild();
+            PerformLinuxBuild();*/
         }
-    }
 
-    [MenuItem( "DevOps/Build/Perform Windows Build" )]
-    public static void PerformWindowsBuild()
-    {
-        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.win/SolarSystem.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
-    }
+        [MenuItem( "DevOps/Build/Perform WebGL Build" )]
+        public static void PerformWebGLBuild()
+        {
+            PlayerSettings.bundleVersion = Git.BuildVersion;
+            var report = BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.webGL", BuildTarget.WebGL, BuildOptions.None);
+            GenerateBuildReport(report);
+        }
+    
+        [MenuItem( "DevOps/Build/Perform macOS Build" )]
+        public static void PerformMacOSBuild()
+        {
+            PlayerSettings.bundleVersion = Git.BuildVersion;
+            BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.osX/SolarSystem.app", BuildTarget.StandaloneOSX, BuildOptions.None);
+            using(ZipFile zip= new ZipFile())
+            {
+                zip.AddFile("../Builds/SolarSystem.osX/SolarSystem.app");
+                zip.Save("../Builds/SolarSystem.osX/SolarSystem.app.zip");
+            }
+        }
 
-    [MenuItem( "DevOps/Build/Perform Linux Builds" )]
-    public static void PerformLinuxBuild()
-    {
-        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.Linux64/SolarSystem.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.None);
-    }
-    [MenuItem( "DevOps/Deploy/itch.io" )]
-    public static void DeployToItchio()
-    {
-        Debug.Log("This will eventually deploy all the builds to itch.io");
+        [MenuItem( "DevOps/Build/Perform Windows Build" )]
+        public static void PerformWindowsBuild()
+        {
+            PlayerSettings.bundleVersion = Git.BuildVersion;
+            BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.win/SolarSystem.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        }
+
+        [MenuItem( "DevOps/Build/Perform Linux Builds" )]
+        public static void PerformLinuxBuild()
+        {
+            PlayerSettings.bundleVersion = Git.BuildVersion;
+            BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, "../Builds/SolarSystem.Linux64/SolarSystem.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.None);
+        }
+        [MenuItem( "DevOps/Deploy/itch.io" )]
+        public static void DeployToItchio()
+        {
+            Debug.Log("This will eventually deploy all the builds to itch.io");
+        }
+
+        private static void GenerateBuildReport(BuildReport report)
+        {
+            var summary = report.summary;
+
+            switch (summary.result)
+            {
+                case BuildResult.Succeeded:
+                    
+                    Debug.Log($"Build succeeded:{summary.totalSize} bytes for {summary.platform}");
+                    break;
+                case BuildResult.Failed:
+                    Debug.Log($"Build failed for {summary.platform}");
+                    break;
+                case BuildResult.Unknown:
+                    break;
+                case BuildResult.Cancelled:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
     }
 }
--- Assets/Scripts/Editor/Build/Git.cs
+++ Assets/Scripts/Editor/Build/Git.cs
@@ -1,4 +1,95 @@
-$HEADER$namespace $NAMESPACE$
+#region MIT License
+
+// # Released under MIT License
+// 
+// Copyright (c) 2021 Byron Wright.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+#endregion
+
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Editor.Build
 {
-  public class $CLASS$ {$END$}
+    /// <summary>
+    ///     GitException includes the error output from a Git.Run() command as well as the
+    ///     ExitCode it returned.
+    /// </summary>
+    public class GitException : InvalidOperationException
+    {
+        /// <summary>
+        ///     The exit code returned when running the Git command.
+        /// </summary>
+        public readonly int ExitCode;
+
+        public GitException(int exitCode, string errors) : base(errors)
+        {
+            ExitCode = exitCode;
+        }
+    }
+
+    public static class Git
+    {
+        /* Properties ============================================================================================================= */
+
+        /// <summary>
+        ///     Retrieves the build version from git based on the most recent matching tag and
+        ///     commit history. This returns the version as: {major.minor.build} where 'build'
+        ///     represents the nth commit after the tagged commit.
+        ///     Note: The initial 'v' and the commit hash code are removed.
+        /// </summary>
+        public static string BuildVersion
+        {
+            get
+            {
+                var version = Run(@"describe --tags --long --match ""v[0-9]*""");
+                // Remove initial 'v' and ending git commit hash.
+                version = version.Replace('-', '.');
+                version = version.Substring(1, version.LastIndexOf('.') - 1);
+                return version;
+            }
+        }
+
+        /// <summary>
+        ///     The currently active branch.
+        /// </summary>
+        public static string Branch => Run(@"rev-parse --abbrev-ref HEAD");
+
+        /// <summary>
+        ///     Returns a listing of all uncommitted or untracked (added) files.
+        /// </summary>
+        public static string Status => Run(@"status --porcelain");
+
+
+        /* Methods ================================================================================================================ */
+
+        /// <summary>
+        ///     Runs git.exe with the specified arguments and returns the output.
+        /// </summary>
+        public static string Run(string arguments)
+        {
+            using (var process = new Process())
+            {
+                var exitCode = process.Run(@"git", arguments, Application.dataPath,
+                    out var output, out var errors);
+                if (exitCode == 0)
+                    return output;
+                throw new GitException(exitCode, errors);
+            }
+        }
+    }
 }
